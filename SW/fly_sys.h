f3a706e625ed34b66b590f8f1ba9910b4d76a2b7
koyunbaba <koyunbaba0421@gmail.com>
2014-02-13 23:39:52 +0800

    Create fly_sys.h

---
+++ SW/fly_sys.h
@@ -0,0 +1,22 @@
+void MoveDiff
+(
+  int d_x, 
+  int d_y, 
+  int d_z, 
+  int d_alpha, 
+  int d_beta, 
+  int d_theta, 
+  int duration
+);
+
+void MoveTo
+(
+  int x, 
+  int y, 
+  int z, 
+  int alpha, 
+  int beta, 
+  int theta, 
+  int duration
+);
+
